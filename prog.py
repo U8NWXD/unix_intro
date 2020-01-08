5b9f176c95a8de8e2e2ed51b2956505720f73f8c
cs <cs.temporary@icloud.com>
2020-01-08 02:03:40 +0000

    Add password generator

--- prog.py
+++ prog.py
@@ -7,10 +7,14 @@
 # TODO: Add huge comment
 
 
+from __future__ import print_function
+
 import argparse
 from datetime import timedelta
 import filecmp
 import os
+from random import SystemRandom
+import sys
 import time
 
 
@@ -29,6 +33,20 @@ def handler_diff(args):
     print(same)
 
 
+def handler_pass(args):
+    if args.seed:
+        print(
+            "WARNING: The password is NOT SECURE because seed set",
+            file=sys.stderr,
+        )
+    rand = SystemRandom(args.seed)
+    with open("/usr/share/dict/words", "r") as f:
+        words = [word.strip() for word in f]
+    password = [rand.choice(words) for _ in range(args.len)]
+    for word in password:
+        print(word)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="A program to help you experiment with some UNIX commands"
@@ -40,11 +58,20 @@ def main():
     parser_edited.set_defaults(func=handler_edited)
     parser_edited.add_argument("file", type=str, action="store")
 
-    parser_edited = subparsers.add_parser(
+    parser_diff = subparsers.add_parser(
         "diff", description="Find whether 2 files are the same")
-    parser_edited.set_defaults(func=handler_diff)
-    parser_edited.add_argument("file1", type=str, action="store")
-    parser_edited.add_argument("file2", type=str, action="store")
+    parser_diff.set_defaults(func=handler_diff)
+    parser_diff.add_argument("file1", type=str, action="store")
+    parser_diff.add_argument("file2", type=str, action="store")
+
+    parser_pass = subparsers.add_parser(
+        "pass", description="Generate a password of random words")
+    parser_pass.set_defaults(func=handler_pass)
+    parser_pass.add_argument("len", type=int, action="store")
+    parser_pass.add_argument(
+        "--seed", type=int, action="store", default=None,
+        help="seed to make behavior deterministic"
+    )
 
     args = parser.parse_args()
     args.func(args)
